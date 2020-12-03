392ffe03084ae24b9a55a2ff7ef4b8de20f1e75f
Mauricio Mesquita <mdeoliveiramesquita@freedommobile.ca>
2020-12-03 18:44:48 -0500

    articles -> typescript props added on headers/css.tsx

--- client/src/components/headers/css.tsx
+++ client/src/components/headers/css.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from "styled-components";
 import { Box } from "@material-ui/core";
 
-//It's not being in use yet
+export interface BoxProps {
+  background: string
+}
 
 export const Header = styled(Box)`
-  background-color: ${props => props.background};
+  background-color: ${(props: BoxProps) => props.background};
 `;
 
