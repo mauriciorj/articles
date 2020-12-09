3dc472c5bbd4cf68b3cf1c0bd927918447e26929
Mauricio Mesquita <mdeoliveiramesquita@freedommobile.ca>
2020-12-08 23:47:08 -0500

    /root ->dded the new repo: clientApi -> articlesRobot name changed to crawler
    client
    -> graphql client added
    
    clientApi
    -> graphql and apollos server added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,13 +3,22 @@ import { ThemeProvider } from "styled-components";
 import theme from "../src/styles/theme";
 import RouterApp from "./routerApp";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.APOLLO_SERVER_URL,
+  cache: new InMemoryCache(),
+});
 
 const HomePage = () => {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <RouterApp />
-      </ThemeProvider>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <RouterApp />
+        </ThemeProvider>
+      </ApolloProvider>
     </BrowserRouter>
   );
 };
