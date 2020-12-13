68207333c618a6ac7de6d8bb9f1b727514634f03
Mauricio Mesquita <mdeoliveiramesquita@freedommobile.ca>
2020-12-13 16:03:40 -0500

    client + add header using next/head + add AMP using export const config = { amp: true }

--- client/src/containers/mainPage/mainPage.tsx
+++ client/src/containers/mainPage/mainPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import { MainHeader } from "../../components/headers/mainHeader";
+import HeaderPage from '../../../pages/headerPage';
+export const config = { amp: true }
 
 export const MainPage = () => {
   return (
     <div>
+      <HeaderPage />
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <MainHeader />
