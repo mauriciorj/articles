040077c3568796e01aa6426d24161e5508c08829
Mauricio Mesquita <mdeoliveiramesquita@freedommobile.ca>
2020-12-18 19:31:14 -0500

    client apollo client added

--- client/src/containers/mainPage/mainPage.tsx
+++ client/src/containers/mainPage/mainPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import { MainHeader } from "../../components/headers/mainHeader";
 import HeaderPage from '../../../pages/headerPage';
+import getArticles from '../../graphql/articles';
+
 export const config = { amp: true }
 
 export const MainPage = () => {
@@ -11,6 +13,7 @@ export const MainPage = () => {
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <MainHeader />
+          {getArticles()}
         </Grid>
       </Grid>
     </div>
