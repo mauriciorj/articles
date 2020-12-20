d1c12d2896f0ea15a8e08943929e47dc8a009bdc
ruvalter <ruvalter@gmail.com>
2020-12-20 13:24:11 -0500

    ART-0101 - Add articles feed

--- client/src/containers/mainPage/mainPage.tsx
+++ client/src/containers/mainPage/mainPage.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
-import { MainHeader } from "../../components/headers/mainHeader";
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { MainHeader } from '../../components/headers/mainHeader';
+import { ArticleCard } from 'wcl_articles/src/components/ArticleCard/ArticleCard';
+import ArticlesFeed from '../../components/articlesFeed/ArticlesFeed';
 
 export const MainPage = () => {
   return (
@@ -8,6 +10,9 @@ export const MainPage = () => {
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <MainHeader />
+          {/* TODO: Create a layout component */}
+          <div style={{ marginBottom: '2rem' }}></div>
+          <ArticlesFeed></ArticlesFeed>
         </Grid>
       </Grid>
     </div>
