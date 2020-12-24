0c035c07aacf2b075107b44fefdd96aef46b8353
Mauricio Mesquita <mdeoliveiramesquita@freedommobile.ca>
2020-12-23 22:18:21 -0500

    Card updated
    web_component_library removed

--- wcl_articles/src/components/ArticleCard/ArticleCard.js
+++ wcl_articles/src/components/ArticleCard/ArticleCard.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react';
-import { ThemeContext } from 'styled-components';
-import Card from '@material-ui/core/Card';
+import React, { useState, useContext, useEffect } from "react";
+import { ThemeContext } from "styled-components";
+import Card from "@material-ui/core/Card";
 
 import {
   Avatar,
@@ -10,14 +10,14 @@ import {
   createStyles,
   IconButton,
   makeStyles,
-} from '@material-ui/core';
+} from "@material-ui/core";
 // import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { red } from '@material-ui/core/colors';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import ShareIcon from '@material-ui/icons/Share';
-import Typography from '@material-ui/core/Typography';
-import { InfoList } from '../InfoList/InfoList';
-import { Link } from 'react-router-dom';
+import { red } from "@material-ui/core/colors";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import ShareIcon from "@material-ui/icons/Share";
+import Typography from "@material-ui/core/Typography";
+import { InfoList } from "../InfoList/InfoList";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -26,24 +26,24 @@ const useStyles = makeStyles((theme) =>
     },
     media: {
       height: 0,
-      paddingTop: '56.25%', // 16:9
+      paddingTop: "56.25%", // 16:9
     },
     expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
         duration: theme.transitions.duration.shortest,
       }),
     },
     expandOpen: {
-      transform: 'rotate(180deg)',
+      transform: "rotate(180deg)",
     },
     avatar: {
       backgroundColor: red[500],
     },
     articleInfo: {
-      fontSize: '14px',
-      color: '#3e3e3e',
+      fontSize: "14px",
+      color: "#3e3e3e",
     },
   })
 );
@@ -60,24 +60,19 @@ export const ArticleCard = ({
   description,
 }) => {
   const classes = useStyles();
-  // const [expanded, setExpanded] = React.useState(false);
-
-  // const handleExpandClick = () => {
-  //   setExpanded(!expanded);
-  // };
   const activeTheme = useContext(ThemeContext);
-  const [saved, setSaved] = useState(favorite);
+  const [saved, setSaved] = useState(false);
 
-  const handleReadMore = () => {
-    console.log('Read more clicked');
-  };
+  useEffect(() => {
+    setSaved(favorite);
+  }, []);
 
   const handleSaving = () => {
     setSaved(!saved);
   };
 
   const handleSharing = () => {
-    console.log('Share button clicked');
+    console.log("Share button clicked");
   };
 
   return (
@@ -85,13 +80,13 @@ export const ArticleCard = ({
       <CardHeader
         avatar={
           <Avatar
-            aria-label='recipe'
+            aria-label="recipe"
             src={sourceImage}
             className={classes.avatar}
           ></Avatar>
         }
         action={
-          <IconButton aria-label='settings'>
+          <IconButton aria-label="settings">
             {/* TODO: Implement more options such as: Remove this category from feed etc. */}
             {/* <MoreVertIcon /> */}
           </IconButton>
@@ -101,25 +96,25 @@ export const ArticleCard = ({
       />
       <CardContent>
         <Link
-          style={{ textDecoration: 'none' }}
+          style={{ textDecoration: "none" }}
           to={{
             pathname: `/articleView/${id}`,
             state: { fromDashboard: true, url },
           }}
         >
           <Typography
-            variant='h5'
-            color='textPrimary'
-            component='h2'
+            variant="h5"
+            color="textPrimary"
+            component="h2"
             gutterBottom={true}
           >
             {title}
           </Typography>
           <Typography
-            style={{ marginBottom: '1rem' }}
-            variant='body2'
-            color='textSecondary'
-            component='p'
+            style={{ marginBottom: "1rem" }}
+            variant="body2"
+            color="textSecondary"
+            component="p"
           >
             {description}
           </Typography>
@@ -128,7 +123,7 @@ export const ArticleCard = ({
       </CardContent>
       <CardActions disableSpacing>
         <IconButton
-          aria-label='add to favorites'
+          aria-label="add to favorites"
           aria-pressed={saved}
           onClick={handleSaving}
         >
@@ -140,7 +135,7 @@ export const ArticleCard = ({
             }}
           />
         </IconButton>
-        <IconButton aria-label='share' onClick={handleSharing}>
+        <IconButton aria-label="share" onClick={handleSharing}>
           <ShareIcon />
         </IconButton>
       </CardActions>
